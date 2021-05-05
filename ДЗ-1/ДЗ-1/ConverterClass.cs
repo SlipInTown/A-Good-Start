9004b946e40014746c626f0dd71f93a99cfced26
Alexander <sashacomp2@gmail.com>
2021-05-05 15:37:34 +0300

    Final

--- ДЗ-1/ДЗ-1/ConverterClass.cs
+++ ДЗ-1/ДЗ-1/ConverterClass.cs
@@ -7,18 +7,18 @@
         private string endedText1 = "Сумма от 1 до N равна ";
 
         private string endedText2 = "Максимальное четное число меньше N равно";
+
+        private string fulltext;
         public void ConvertTuple((int, int, int) tuple)
         {
-
-            var tempText = 
-            $"{endedText} {tuple.Item1} " +
+            fulltext = $"{endedText} {tuple.Item1} " +
             $"\n{endedText1} {tuple.Item2} " +
             $"\n{endedText2} {tuple.Item3} ";
         }
 
         public string ReturnString()
         {
-            return endedText;
+            return fulltext;
         }
     }
 }
